a5587fbb4afc4e5f116bca51e7c44a52afe32840
Kazuki Otsu <cocoeyes02@gmail.com>
2022-07-24 21:50:27 +0900

    test(Franc): 第5章で初めてREDになるところまで写経した
    P29 ~ 30 を参照

--- app/money/tests/Unit/MoneyTest.php
+++ app/money/tests/Unit/MoneyTest.php
@@ -20,6 +20,12 @@ class MoneyTest extends TestCase
     {
         $this->assertTrue((new Dollar(5))->equals(new Dollar(5)));
         $this->assertFalse((new Dollar(5))->equals(new Dollar(6)));
+    }
 
+    public function testFrancMultiplication()
+    {
+        $five = new Franc(5);
+        $this->assertObjectEquals(new Franc(10), $five->times(2));
+        $this->assertObjectEquals(new Franc(15), $five->times(3));
     }
 }
