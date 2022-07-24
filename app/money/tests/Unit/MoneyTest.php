f92cebdac5b789157e3b1facf0ad93c3a33c085a
Kazuki Otsu <cocoeyes02@gmail.com>
2022-07-24 21:35:33 +0900

    test(Dollar): プロパティをprivateにするため、プロパティ同士の比較ではなく等価性比較によるメソッドを使うよう変更
    P25 ~ 26を参照

--- app/money/tests/Unit/MoneyTest.php
+++ app/money/tests/Unit/MoneyTest.php
@@ -12,10 +12,8 @@ class MoneyTest extends TestCase
     public function testMultiplication()
     {
         $five = new Dollar(5);
-        $product = $five->times(2);
-        $this->assertSame(10, $product->amount);
-        $product = $five->times(3);
-        $this->assertSame(15, $product->amount);
+        $this->assertObjectEquals(new Dollar(10), $five->times(2));
+        $this->assertObjectEquals(new Dollar(15), $five->times(3));
     }
 
     public function testEquality()
