3b4892d1dcbe6875d5a546f7da0378ee46ec5620
Kazuki Otsu <cocoeyes02@gmail.com>
2022-08-01 13:50:37 +0900

    feat(Money): Moneyにdollarメソッドとfrancメソッドを生やすことで、Factory Methodパターンで存在を隠した

--- app/money/src/Money.php
+++ app/money/src/Money.php
@@ -4,13 +4,25 @@ declare(strict_types=1);
 
 namespace Money;
 
-class Money
+abstract class Money
 {
     protected int $amount;
+
+    abstract function times(int $multiplier): self;
 
     public function equals(self $money): bool
     {
         return $this->amount === $money->amount
             && $this::class === $money::class;
+    }
+
+    static function dollar(int $amount): self
+    {
+        return new Dollar($amount);
+    }
+
+    static function franc(int $amount): self
+    {
+        return new Franc($amount);
     }
 }
