1cc042845ca7d48f622988d1c320d18c6c3d5025
Kazuki Otsu <cocoeyes02@gmail.com>
2022-08-01 14:46:28 +0900

    refactor(times): Moneyではなくサブクラスに元に戻した

--- app/money/src/Dollar.php
+++ app/money/src/Dollar.php
@@ -8,6 +8,6 @@ class Dollar extends Money
 {
     public function times(int $multiplier): Money
     {
-        return new Money($this->amount * $multiplier, $this->currency);
+        return new Dollar($this->amount * $multiplier, $this->currency);
     }
 }
