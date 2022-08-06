78d88434e0768ec82b46608869e5ecc0a4b8e16a
Kazuki Otsu <cocoeyes02@gmail.com>
2022-08-06 22:56:42 +0900

    test(Bank): Bank@reduceを実装するため、先にテストコードを書いた（これだとまだRED状態）

--- app/money/tests/Unit/MoneyTest.php
+++ app/money/tests/Unit/MoneyTest.php
@@ -49,4 +49,12 @@ class MoneyTest extends TestCase
         $this->assertObjectEquals($five, $sum->augend);
         $this->assertObjectEquals($five, $sum->addend);
     }
+
+    public function testReduceSum()
+    {
+        $sum = new Sum(Money::dollar(3), Money::dollar(4));
+        $bank = new Bank();
+        $result = $bank->reduce($sum, "USD");
+        $this->assertObjectEquals(Money::dollar(7), $result);
+    }
 }
