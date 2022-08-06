87e1d4dc25f05e066a12445338ac14501d57beae
Kazuki Otsu <cocoeyes02@gmail.com>
2022-08-06 17:03:55 +0900

    test(Bank): Bankの概念をテストコードに反映させた
    ついてでにreduceメソッドも

--- app/money/tests/Unit/MoneyTest.php
+++ app/money/tests/Unit/MoneyTest.php
@@ -31,7 +31,10 @@ class MoneyTest extends TestCase
 
     public function testSimpleAddition()
     {
+        $bank = new Bank();
         $sum = Money::dollar(5)->plus(Money::dollar(5));
         $this->assertObjectEquals(Money::dollar(10), $sum);
+        $reduced = $bank->reduce($sum, "USD");
+        $this->assertObjectEquals(Money::dollar(10), $reduced);
     }
 }
