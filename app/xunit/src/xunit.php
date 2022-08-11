6eb1eaeb5718b44ebd931260663c2d5085845f70
Kazuki Otsu <cocoeyes02@gmail.com>
2022-08-11 18:45:40 +0900

    feat(xunit): とりあえずGreen状態に持っていくため、テストメソッドを仮実装した

--- app/xunit/src/xunit.php
+++ app/xunit/src/xunit.php
@@ -2,6 +2,21 @@
 
 declare(strict_types=1);
 
+class WasRun
+{
+    public ?int $wasRun;
+
+    public function __construct($name)
+    {
+        $this->wasRun = null;
+    }
+
+    public function testMethod(): void
+    {
+        $this->wasRun = 1;
+    }
+}
+
 ini_set('assert.active', '1');
 ini_set('assert.exception', '1');
 
